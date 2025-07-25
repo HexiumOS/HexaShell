d4a468a2ead77c6c9bf7a63677cb3d383fab9e2b
Viktor Popp <ViktorPopp.dev@gmail.com>
2025-07-25 14:47:56 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::{
+    io::{self, Write},
+    process::{Command, Stdio, exit},
+};
+
+fn main() {
+    loop {
+        print!("hexsh> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        if io::stdin().read_line(&mut input).is_err() {
+            eprintln!("Failed to read user input");
+            continue;
+        }
+
+        if input.trim().is_empty() {
+            continue;
+        }
+
+        if input.trim() == "exit" {
+            exit(0);
+        }
+
+        let mut parts = input.split_whitespace();
+        let command = match parts.next() {
+            Some(cmd) => cmd,
+            None => continue,
+        };
+        let args: Vec<&str> = parts.collect();
+
+        let mut child = match Command::new(command)
+            .args(&args)
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+        {
+            Ok(child) => child,
+            Err(e) => {
+                eprintln!("Error executing {}: {}", command, e);
+                continue;
+            }
+        };
+
+        if let Err(e) = child.wait() {
+            eprintln!("Failed to work on child process: {}", e);
+        }
+    }
+}
